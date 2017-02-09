0b7014399472843b247c3c4eb07845f1c9496d8c
slypsy <tamarakovacev13@gmail.com>
2017-02-09 19:11:57 +0100

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var htmlImport = require('gulp-html-import');
+var browserSync = require('browser-sync').create();
+
+gulp.task('sass', function () {
+    return gulp.src('./sass/**/*.scss')
+        .pipe(sass({
+            //outputStyle: 'compressed',
+            includePaths: [
+                'node_modules/breakpoint-sass/stylesheets'
+            ]
+        }).on('error', sass.logError))
+        .pipe(gulp.dest('./dist/css'))
+        .pipe(browserSync.stream());
+});
+
+// gulp.task('clean-css', function () {
+//     return gulp.src(['./css','./dist/css'], {read: false}).pipe(clean());
+// });
+
+gulp.task('html-import', function () {
+    return gulp.src('./src/*.html')
+        .pipe(htmlImport('./src/components/'))
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('default', ['sass'], function() {
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+
+    gulp.watch('./sass/**/*.scss', [/*'clean-css',*/'sass']);
+    gulp.watch('./src/**/*.html', ['html-import']);
+    gulp.watch('./dist/**/*.html', browserSync.reload);
+});
+
